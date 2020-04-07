74397b056bdf0c499e83f415181855759ccc0119
Tom Nash <contacttomnash@gmail.com>
2020-04-07 14:48:28 +1000

    How to coming soon

--- src/archetypes/Pages/Docs.svelte
+++ src/archetypes/Pages/Docs.svelte
@@ -2,15 +2,15 @@
 	import PanelLayout from '@layouts/Panel.svelte'
 </script>
 
-<PanelLayout 
+<PanelLayout
 	header={{
 		title: 'Docs',
 		icon: 'menu_book',
 	}}
 	showBreadcrumbs
 	>
-	<h2 class="mdc-typography--headline6">Excepteur sint occaecat</h2>
+	<h2 class="mdc-typography--headline6">How To</h2>
 	<p class="mdc-typography--body1 -content">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+		Coming soon!
 	</p>
 </PanelLayout>
