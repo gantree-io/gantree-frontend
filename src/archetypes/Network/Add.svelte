874eed72165e3b3bb26f8295070f847767c88dbd
Tom Nash <contacttomnash@gmail.com>
2020-04-14 10:54:06 +1000

    Fix a couple of tooltips

--- src/archetypes/Network/Add.svelte
+++ src/archetypes/Network/Add.svelte
@@ -246,7 +246,7 @@
 
 				<Field
 					title={`Binary optons (${_.get(_fields, 'binary_opts', []).length})`}
-					help="Binary options will be added to the binary at compile time"
+					help="Binary options will be added to the binary at execution time"
 					input={{
 						id: 'binary_opts',
 						type: 'pills',
