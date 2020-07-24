c41cd7fa6318e5c095fd64dae9c4b4e29fd94ab2
Tom Nash <contacttomnash@gmail.com>
2020-07-24 17:28:51 +1000

    Gantree-node-watchdog

--- src/archetypes/Network/Index.svelte
+++ src/archetypes/Network/Index.svelte
@@ -63,7 +63,7 @@
       {:else}
         <NoResults graphic={NotFound} title="No networks available">
           Start by installing
-          <a href="https://github.com/gantree-io/gantree-monitor">
+          <a href="https://github.com/gantree-io/gantree-node-watchdog">
             Gantree Monitor
           </a>
           on one or more of your nodes.
--- src/archetypes/Pages/ApiKeys.svelte
+++ src/archetypes/Pages/ApiKeys.svelte
@@ -26,7 +26,7 @@
   <p class="mdc-typography--body1 -content">
     At the moment you have a single, unmodifiable API Key. You should use this
     when you set up the
-    <a href="https://github.com/gantree-io/gantree-monitor" target="_blank">
+    <a href="https://github.com/gantree-io/gantree-node-watchdog" target="_blank">
       Gantree Monitor
     </a>
     on each of your nodes.
