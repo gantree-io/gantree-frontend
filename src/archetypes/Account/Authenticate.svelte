21a368f0b527a2ad1cce06fd593ba34a2a6bed6f
Daniel Harrison <daniel.equiknox@gmail.com>
2020-11-26 17:06:21 +1100

    Authenticate page vertical alignment fix

--- src/archetypes/Account/Authenticate.svelte
+++ src/archetypes/Account/Authenticate.svelte
@@ -2,6 +2,7 @@
   import { push } from 'svelte-spa-router'
   import { Icon } from '@smui/common';
   import Centered from '@layouts/Centered.svelte'
+  import GantreeLogo from '@assets/GantreeLogo.svelte'
   import AccountStore, { AuthStatus } from '@archetypes/Account/store'
 
   AccountStore.subscribe(({authStatus}) => {
@@ -10,6 +11,14 @@
 </script>
 
 <style lang="scss">
+  :global(.layout.-centered.-authenticate) {
+    justify-content: center !important;
+
+    :global(.children) > :global(.gantree-logo){
+       margin: 0 0 0.5em 0;
+    }
+  }
+
   h1{
     margin: 0;
   }
@@ -24,8 +33,8 @@
   }
 </style>
 
-<Centered>
-  <h1 class='mdc-typography--headline3'>Gantree</h1>
+<Centered class={'-authenticate'}>
+  <GantreeLogo/>
   <h2 class='mdc-typography--headline5'>Authenticating</h2>
   <Icon class="material-icons thumb -animation-pulse">fingerprint</Icon>
   <p class="mdc-typography--body1">You will be redirected to <strong>Gantree</strong> once authenticated</p>
